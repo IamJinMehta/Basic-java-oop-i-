6861999c8baf44841322353146b4dd9b674d1fef
JINMETA <56715909+jinmeta@users.noreply.github.com>
2019-12-13 19:29:57 +0530

    GTU

---
+++ programe 1.java
@@ -0,0 +1,8 @@
+/*Write a Program that displays Welcome to Java, Learning Java Now and Programming is fun*/
+class HelloWorld 
+{ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Welcome to Java, Learning Java Now and Programming is fun"); 
+    } 
+} 
