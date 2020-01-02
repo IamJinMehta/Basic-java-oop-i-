286c2972da47279a27b06dac34cf3807856bf7e1
JINMETA <56715909+jinmeta@users.noreply.github.com>
2020-01-02 21:39:28 +0530

    GTU

---
+++ program8.java
@@ -0,0 +1,26 @@
+//Write a program that reads an integer and displays all its smallest factors in increasing order.
+//For example if input number is 120, the output should be as follows:2,2,2,3,5. 
+import java.util.Scanner;
+public class FactorFinder
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("please enter an integer: ");
+		int num = input.nextInt();
+		int identifier = 2;
+		while(num/identifier != 1)
+		{
+			if(num % identifier == 0)
+				{	
+				System.out.print(identifier + ",");
+				num = num/identifier;
+				}
+				else
+				{
+				identifier++;	
+				}
+			}
+			System.out.println(num + ".");
+		}
+	}
