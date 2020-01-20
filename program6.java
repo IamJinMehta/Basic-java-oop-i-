82a9aa14fa609c3beccda65133e61001b208d865
JINMETA <56715909+jinmeta@users.noreply.github.com>
2020-01-20 20:53:53 +0530

    GTU

--- program6.java
+++ program6.java
@@ -7,13 +7,17 @@ public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 
 System.out.print("Enter the letter: ");
-char latter = scan.next().charAt(0)
-	;
+char latter = scan.next().charAt(0);
 if ((latter == 'a')||(latter == 'e')||(latter == 'i')||(latter == 'o')||(latter == 'u'))
     {
     System.out.println ("latter is vowel");
     }
-else{
+else if ((latter == 'A')||(latter == 'E')||(latter == 'I')||(latter == 'O')||(latter == 'U'))
+    {
+    System.out.println ("latter is vowel");
+    }
+    else
+    {
     System.out.println("latter is consonant");
     }
     }
